059eafa664685315eac1d68ac17b1a71fd2611c5
sporter <sporter@ownapt.com>
2016-09-23 08:57:03 -0600

    Added string extensions

---
+++ src/OwnApt/Extension/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace OwnApt.Common.Extension
+{
+    public static class StringExtensions
+    {
+        #region Public Methods
+
+        public static string ValueIfNullOrEmpty(this string str, string value)
+        {
+            return string.IsNullOrEmpty(str) ? value : str;
+        }
+
+        public static string ValueIfNullOrWhitespace(this string str, string value)
+        {
+            return string.IsNullOrWhiteSpace(str) ? value : str;
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ test/OwnApt.Tests.Component/Extension/StringExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿using OwnApt.Common.Extension;
+using Xunit;
+
+namespace OwnApt.Tests.Component.Extension
+{
+    public class StringExtensionTests
+    {
+        #region Public Methods
+
+        [Fact]
+        public void CanGetValueWithEmptyOrWhitespaceString()
+        {
+            var emptyString = "";
+            var whitespaceString = "               ";
+
+            var stringFromNullOrEmptyCheck = emptyString.ValueIfNullOrEmpty("NullOrEmpty");
+            var stringFromNullOrWhitespaceCheck = whitespaceString.ValueIfNullOrWhitespace("NullOrWhitespace");
+
+            Assert.NotNull(emptyString);
+            Assert.NotNull(whitespaceString);
+            Assert.NotNull(stringFromNullOrEmptyCheck);
+            Assert.NotNull(stringFromNullOrWhitespaceCheck);
+            Assert.Equal(stringFromNullOrEmptyCheck, "NullOrEmpty");
+            Assert.Equal(stringFromNullOrWhitespaceCheck, "NullOrWhitespace");
+        }
+
+        [Fact]
+        public void CanGetValueWithNullString()
+        {
+            string nullString = null;
+
+            var stringFromNullOrEmptyCheck = nullString.ValueIfNullOrEmpty("NullOrEmpty");
+            var stringFromNullOrWhitespaceCheck = nullString.ValueIfNullOrWhitespace("NullOrWhitespace");
+
+            Assert.Null(nullString);
+            Assert.NotNull(stringFromNullOrEmptyCheck);
+            Assert.NotNull(stringFromNullOrWhitespaceCheck);
+            Assert.Equal(stringFromNullOrEmptyCheck, "NullOrEmpty");
+            Assert.Equal(stringFromNullOrWhitespaceCheck, "NullOrWhitespace");
+        }
+
+        #endregion Public Methods
+    }
+}
