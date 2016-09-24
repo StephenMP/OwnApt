2a6004b1a5a058d5b0a593beee95b90b88b5656b
sporter <sporter@ownapt.com>
2016-09-23 21:31:27 -0600

    Added lease period status enum

---
+++ src/OwnApt/Enum/LeasePeriodStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OwnApt.Common.Enum
+{
+    public enum LeasePeriodStatus
+    {
+        Unknown = 0,
+        PaymentDue = 1,
+        PaymentReceived = 2,
+        Closed = 3
+    }
+}
